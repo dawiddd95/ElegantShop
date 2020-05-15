7ad5291c8acf48c7501d8cef29e241adbb1c2c31
dawiddd95 <dawlyc@wp.pl>
2020-05-15 15:37:11 +0200

    UserDao api removeUserById/editUserById methods removed

--- src/main/java/com/elegantshop/api/UserDao.java
+++ src/main/java/com/elegantshop/api/UserDao.java
@@ -7,9 +7,7 @@ import java.util.List;
 public interface UserDao {
     void saveUser(User user);
 
-    void removeUserById(Long id);
-    void editUserById(Long id);
-
     List<User> getAllUsers();
     User getUserById(Long id);
 }
+
