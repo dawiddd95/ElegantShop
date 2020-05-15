a332efdc4019c707d130042438d3a4c3a24341db
dawiddd95 <dawlyc@wp.pl>
2020-05-15 16:38:50 +0200

    UserServiceImpl service updated

--- src/main/java/com/elegantshop/service/UserServiceImpl.java
+++ src/main/java/com/elegantshop/service/UserServiceImpl.java
@@ -1,5 +1,41 @@
 package com.elegantshop.service;
 
-public class UserServiceImpl {
+import com.elegantshop.api.UserDao;
+import com.elegantshop.api.UserService;
+import com.elegantshop.dao.UserDaoImpl;
+import com.elegantshop.model.User;
 
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private static UserServiceImpl instance = null;
+    private UserDao userDao = UserDaoImpl.getInstance();
+
+    private UserServiceImpl() { }
+
+    public static UserServiceImpl getInstance() {
+        if (instance == null) {
+            instance = new UserServiceImpl();
+        }
+        return instance;
+    }
+
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    public User getUserById(Long userId) {
+        List<User> users = getAllUsers();
+
+        for (User user : users
+        ) {
+            boolean isFoundUser = user.getId().equals(userId);
+            if (isFoundUser) {
+                return user;
+            }
+
+        }
+
+        return null;
+    }
 }
