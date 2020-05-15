da97c7121f0299006cc4448586bcaa838f66ef9c
dawiddd95 <dawlyc@wp.pl>
2020-05-15 15:35:13 +0200

    UserDao api updated

--- src/main/java/com/elegantshop/api/UserDao.java
+++ src/main/java/com/elegantshop/api/UserDao.java
@@ -1,6 +1,15 @@
 package com.elegantshop.api;
 
+import com.elegantshop.model.User;
+
+import java.util.List;
+
 public interface UserDao {
-    // TODO
-    //  dodawanie, usuwanie, edycja, pobranie wszystkich, pobranie po id
+    void saveUser(User user);
+
+    void removeUserById(Long id);
+    void editUserById(Long id);
+
+    List<User> getAllUsers();
+    User getUserById(Long id);
 }
