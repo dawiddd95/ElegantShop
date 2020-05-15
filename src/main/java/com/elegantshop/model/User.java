11e442e056df300ac8fb8a34a779cb26ed2ddfcb
dawiddd95 <dawlyc@wp.pl>
2020-05-15 13:01:31 +0200

    User model updated

--- src/main/java/com/elegantshop/model/User.java
+++ src/main/java/com/elegantshop/model/User.java
@@ -1,5 +1,32 @@
 package com.elegantshop.model;
 
 public class User {
+    private long id;
+    private String login;
+    private String password;
 
+    private final static String USER_SEPARATOR = " || ";
+
+    public User(long id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return id + USER_SEPARATOR + login + USER_SEPARATOR + password;
+    }
 }
