6e9140b3ad2ae1ca16c65363b576bf912869a741
dawiddd95 <dawlyc@wp.pl>
2020-05-15 13:27:36 +0200

    UserService api updated

--- src/main/java/com/elegantshop/api/UserService.java
+++ src/main/java/com/elegantshop/api/UserService.java
@@ -1,4 +1,14 @@
 package com.elegantshop.api;
 
+import com.elegantshop.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    boolean addUser(User user);
+
+    List<User> getAllUsers();
+    User getUserByLogin(String login);
+
+    boolean isCorrectLoginAndPassword(String login, String password);
 }
