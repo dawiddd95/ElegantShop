4974c1c0134bc7a12c857fabfa2c90962e539aee
dawiddd95 <dawlyc@wp.pl>
2020-05-16 16:43:46 +0200

    ColorShoes enum create

---
+++ src/main/java/com/elegantshop/model/enums/ColorShoes.java
@@ -0,0 +1,13 @@
+package com.elegantshop.model.enums;
+
+public enum ColorShoes {
+    BLACK("Black"),
+    WHITE("White"),
+    BROWN("Brown");
+
+    private String color;
+
+    ColorShoes(String color) {
+        this.color = color;
+    }
+}
