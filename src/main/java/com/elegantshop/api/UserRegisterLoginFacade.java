665c8ee270640c335b75747d09ba548acb66f819
dawiddd95 <dawlyc@wp.pl>
2020-05-16 13:44:27 +0200

    UserRegisterLoginFacade api getAllUsers method added

--- src/main/java/com/elegantshop/api/UserRegisterLoginFacade.java
+++ src/main/java/com/elegantshop/api/UserRegisterLoginFacade.java
@@ -2,7 +2,11 @@ package com.elegantshop.api;
 
 import com.elegantshop.model.User;
 
+import java.util.List;
+
 public interface UserRegisterLoginFacade {
     boolean registerUser(User user);
     boolean loginUser(String login, String password);
+
+    List<User> getAllUsers();
 }
