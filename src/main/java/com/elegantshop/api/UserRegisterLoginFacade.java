fbe12d119168a7dd8434bdf5187d82dedfb2cba1
dawiddd95 <dawlyc@wp.pl>
2020-05-16 13:14:25 +0200

    UserRegisterLoginFacade api updated

--- src/main/java/com/elegantshop/api/UserRegisterLoginFacade.java
+++ src/main/java/com/elegantshop/api/UserRegisterLoginFacade.java
@@ -1,4 +1,8 @@
 package com.elegantshop.api;
 
+import com.elegantshop.model.User;
+
 public interface UserRegisterLoginFacade {
+    boolean registerUser(User user);
+    boolean loginUser(String login, String password);
 }
