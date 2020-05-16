d8fcbc573bae853859bef16a1f6a7b1ece696ccf
dawiddd95 <dawlyc@wp.pl>
2020-05-16 16:40:28 +0200

    Shoes model update

--- src/main/java/com/elegantshop/model/Shoes.java
+++ src/main/java/com/elegantshop/model/Shoes.java
@@ -1,4 +1,18 @@
 package com.elegantshop.model;
 
-public class Shoes {
+import com.elegantshop.model.enums.SkinType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Shoes extends LowerBodyProduct {
+
+    public Shoes(String mark, String model, BigDecimal price, int productCount, LocalDateTime createdAt, SkinType skinType, int size) {
+        super(mark, model, price, productCount, createdAt, skinType, size);
+    }
+
+    @Override
+    public String toString() {
+        return getBasicProductData()+", Skin type: "+this.getSkinType()+", Size: "+this.getSize();
+    }
 }
